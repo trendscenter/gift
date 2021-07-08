261d1ef9bfb8f5c654329224b424c5298e9584fe
calhounlab <33673667+calhounlab@users.noreply.github.com>
2021-07-08 16:57:00 -0400

    Add files via upload
    get data function is updated to use adaptive iva

--- GroupICATv4.0c/icatb/icatb_helper_functions/icatb_getDataForICA.m
+++ GroupICATv4.0c/icatb/icatb_helper_functions/icatb_getDataForICA.m
@@ -30,7 +30,7 @@ if (strcmpi(modalityType, 'fmri'))
     end
 end
 
-if (~strcmpi(algorithmName, 'iva-gl') && ~strcmpi(algorithmName, 'iva-l') && ~strcmpi(algorithmName, 'iva-l-sos'))
+if (isempty(icatb_findstr(lower(algorithmName),'iva')))
     
     pcain = [sesInfo.data_reduction_mat_file,num2str(sesInfo.numReductionSteps),'-',num2str(1), '.mat'];
     
